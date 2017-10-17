a4c8a45a621611871166120ca2a63dfcb8dd8a99
Johannes Elmnäinen <johannes.elmnainen@cs.helsinki.fi>
2017-10-17 17:54:24 +0300

    fine tuning

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
+import sys
 import os
 import requests
 import re
 
 def main():
     baseUrl = 'https://keliapu.net/data/'
-    startYear = 2017
+    startYear = int(sys.argv[1])
+    startMonth = int(sys.argv[2])
+    print("Beginning data collection, starting from " + str(startYear) + "/" + str(startMonth))
     dataDirs = getLinksWithForm(baseUrl, re.compile(r'\d{4}-\d{2}/'))
-    wantedDirs = filterDirs(dataDirs, startYear)
+    wantedDirs = filterDirs(dataDirs, startYear, startMonth)
+    print(wantedDirs)
     fileAddrs = getCsvLinks(baseUrl, wantedDirs)
-    print(fileAddrs)
     downloadFiles('https://keliapu.net/data/', 'data/', fileAddrs)
+    print("Great success!")
 
 # Takes url and regex, return list of links on url where hrefs satisfy the regex
 def getLinksWithForm(url, regex):
@@ -21,8 +25,8 @@ def getLinksWithForm(url, regex):
 
 # Takes list of strings where first 4 letters form an integer and returns all
 # members that are greater than startYear
-def filterDirs(dirs, startYear):
-    return [d for d in dirs if int(d[:4]) >= startYear]
+def filterDirs(dirs, startYear, startMonth):
+    return [d for d in dirs if int(d[:4]) > startYear or (int(d[:4]) == startYear and int(d[5:7]) >= startMonth)]
 
 # Takes a url and a list of URLs and returns a dictionary of URL: [url1,...,urln]
 # links to .csv-files on those URLs
@@ -36,6 +40,7 @@ def downloadFiles(baseUrl, destFolder, dirs):
         assureFolderExists(destFolder + folder)
         for file in files:
             filename = folder + file
+            print("Downloading " + filename)
             content = requests.get(baseUrl + filename).content
             with open(destFolder + filename, 'wb+') as f:
                 f.write(content)
